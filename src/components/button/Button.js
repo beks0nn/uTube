a2deb19689fde6c91d11bb0083ee8ace7cce338f
mm <maxmartfo@gmail.com>
2020-01-17 21:24:04 +0100

    init

---
+++ src/components/button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Button.scss';
+
+function Button() {
+  return (
+    <div className="button"> lol</div>
+  );
+}
+
+export default Button;
---
+++ src/components/player/Player.js
@@ -0,0 +1,24 @@
+
+
+import React from 'react';
+
+import './Player.scss';
+
+const BASE_EMBED_URL = 'https://www.youtube.com/embed/';
+
+function Player({id}) {
+    const embedUrl = `${BASE_EMBED_URL}${id}?autoplay=1&version=3&loop=1&playlist=${id}`;
+    return (
+        <div className="player-container">
+            <div className="video">
+                <iframe title="someplayer" src={embedUrl} frameBorder='0'
+                allow='autoplay; encrypted-media' allowFullScreen />
+            </div> 
+        </div>
+
+    );
+}
+
+export default Player;
+
+
